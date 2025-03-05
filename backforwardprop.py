977794e0b8e7e26b48fa0b3e5dfc1098fa0683f4
Yohannes Taye <ytaye715@gmail.com>
2025-03-05 13:46:36 +0300

    adding backprop function

--- backforwardprop.py
+++ backforwardprop.py
@@ -1,2 +1,37 @@
 # Implement backpropagation for the model previously built using forward propagation, and 
 # evaluate the differences in performance between the two approaches.
+import numpy as np
+
+def backward_propagation(X, y, Z1, A1, Z2, A2, Z3, A3, Z4, output, W1, b1, W2, b2, W3, b3, W4, b4, learning_rate):
+
+    m = X.shape[0]
+    loss = -np.sum(np.log(output[np.arange(m), y] + 1e-15)) / m
+    total_loss = loss
+
+    dZ4 = output
+    dZ4[np.arange(m), y] -= 1
+    dW4 = np.dot(A3.T, dZ4) / m
+    db4 = np.sum(dZ4, axis=0, keepdims=True) / m
+    dA3 = np.dot(dZ4, W4.T)
+    dZ3 = dA3 * (Z3 > 0)
+    dW3 = np.dot(A2.T, dZ3) / m
+    db3 = np.sum(dZ3, axis=0, keepdims=True) / m
+    dA2 = np.dot(dZ3, W3.T)
+    dZ2 = dA2 * (Z2 > 0)
+    dW2 = np.dot(A1.T, dZ2) / m
+    db2 = np.sum(dZ2, axis=0, keepdims=True) / m
+    dA1 = np.dot(dZ2, W2.T)
+    dZ1 = dA1 * (Z1 > 0)
+    dW1 = np.dot(X.T, dZ1) / m
+    db1 = np.sum(dZ1, axis=0, keepdims=True) / m
+
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+    W3 -= learning_rate * dW3
+    b3 -= learning_rate * db3
+    W4 -= learning_rate * dW4
+    b4 -= learning_rate * db4
+
+    return W1, b1, W2, b2, W3, b3, W4, b4, total_loss
